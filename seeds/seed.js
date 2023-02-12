d294ae832d83b054d77158aacb15dbf64f48bc32
jarrettbutler <jbutler98@hotmail.com>
2023-02-11 20:13:28 -0400

    Adding the seeds to the DB

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,8 +1,10 @@
 const sequelize = require('../config/connection');
-const { User, Project } = require('../models');
+const { User, Skill, Role, UserSkill } = require('../models');
 
 const userData = require('./userData.json');
-const projectData = require('./projectData.json');
+const skillData = require('./skillData.json');
+const roleData = require('./roleData.json'); 
+const userSkillData = require('./userSkillData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -12,9 +14,23 @@ const seedDatabase = async () => {
     returning: true,
   });
 
-  for (const project of projectData) {
-    await Project.create({
-      ...project,
+  for (const skill of skillData) {
+    await Skill.create({
+      ...skill,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
+
+  for (const role of roleData) {
+    await Role.create({
+      ...role,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
+
+  for (const userSkill of userSkillData) {
+    await UserSkill.create({
+      ...userSkill,
       user_id: users[Math.floor(Math.random() * users.length)].id,
     });
   }
