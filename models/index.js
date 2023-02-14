42deab951ff681f2ade70d2515cbdbc65fe412b3
jarrettbutler <jbutler98@hotmail.com>
2023-02-13 21:23:30 -0400

    Fixing models and seeds for develop

--- models/index.js
+++ models/index.js
@@ -1,24 +1,53 @@
 const User = require('./User');
 const Role = require('./Role');
-const Skill = require('./Skill')
-const UserSkill = require('./userSkill')
+const Skill = require('./Skill');
+const UserSkill = require('./UserSkill');
 
-User.belongsTo(Role, {
-  foreignKey: 'user_id',
+//Connecting the role and user
+Role.hasMany(User, {
+  foreignKey : 'role_id'
 });
 
+User.belongsTo(Role, {
+  foreignKey: "role_id"
+});
+
+//Connecting the user and user skill
+User.hasMany(UserSkill, {
+  foreignKey: "user_id"
+});
+
+UserSkill.belongsTo(User, {
+  foreignKey: "user_id"
+});
+
+//Connecting skill and userskill
+Skill.hasMany(UserSkill, {
+  foreignKey: "skill_id"
+});
+
+UserSkill.belongsTo(Skill, {
+  foreignKey: "skill_id"
+});
+
+//Connecting the user and skill through userskill
 User.belongsToMany(Skill, {
   through: UserSkill,
   foreignKey: 'user_id',
-})
+});
 
 Skill.belongsToMany(User, {
   through: UserSkill,
   foreignKey: 'skill_id'
-})
-
-Role.hasMany(Skill, {
-  foreignKey: 'role_id',
 });
+
+//Connecting role and skill
+Role.hasMany(Skill, {
+  foreignKey: "role_id"
+});
+
+Skill.belongsTo(Role, {
+  foreignKey: "role_id"
+})
 
 module.exports = { User, Role, Skill, UserSkill };
--- seeds/seed.js
+++ seeds/seed.js
@@ -1,19 +1,26 @@
 const sequelize = require('../config/connection');
 const { User, Skill, Role, UserSkill } = require('../models');
+
 const userData = require('./userData.json');
 const skillData = require('./skillData');
-const roleData = require('./roleData.json');
+const roleData = require('./roleData.json'); 
 const userSkillData = require('./userSkillData.json');
+
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
+
   await Role.bulkCreate(roleData);
+
   const users = await User.bulkCreate(userData, {
     individualHooks: true,
     returning: true,
   });
+  
   await Skill.bulkCreate(skillData);
+  
   await UserSkill.bulkCreate(userSkillData)
 
   process.exit(0);
 };
+
 seedDatabase();
