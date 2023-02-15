11d503ad04e73419a9fe2b5d400dc759536cf431
jarrettbutler <jbutler98@hotmail.com>
2023-02-14 20:41:16 -0400

    Init commit to heroku

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -8,7 +8,7 @@ router.get('/', async (req, res) => {
     // Get all the required data to be displayed in the home page
 
     // Pass serialized data and session flag into template
-    res.render('homepage', {       
+    res.render('home', {       
       logged_in: req.session.logged_in 
     });
   } catch (err) {
